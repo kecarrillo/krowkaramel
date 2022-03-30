35cd4af929252cbc3a016f8cdfc588afb686b03d
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-03-30 15:40:32 +0200

    update client

--- accounts/models.py
+++ accounts/models.py
@@ -2,15 +2,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# GENDER_CHOICES = (
-#     ('M', 'Male'),
-#     ('F', 'Female')
-# )
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female')
+)
 
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
-    pass
-    # phone = models.CharField(max_length=16)
-    # gender = models.ChoiceField(choices=GENDER_CHOICES, widget=models.RadioSelect())
+    phone = models.CharField(max_length=16, default='')
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_CHOICES = [(GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female')]
+    gender = models.IntegerField(choices=GENDER_CHOICES, default='')
+    STATUS_PARTICULIER = 0
+    STATUS_ENTREPRISE = 1
+    STATUS_CHOICES = [(STATUS_PARTICULIER , 'particulier'), (STATUS_ENTREPRISE, 'entreprise')]
+    status = models.IntegerField(choices=STATUS_CHOICES, default='')
